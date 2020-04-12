8cf8d1357a283b555dd1386b55fb03042efc269b
Debasish Ray Chawdhuri <debasish@geekyarticlesc.com>
2020-04-12 23:19:52 +0530

    updated to use linearalgebra library

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 extern crate linearalgebra;
-
+use linearalgebra::Matrix;
+use linearalgebra::field::ComplexField;
+use num_complex::Complex;
 
 fn pow2(x:u8) -> u8 {
     1<<x
 }
 
-fn get_column_for_one(size:u8, one_position: u8) -> Vec<u8> {
+fn get_column_for_one(size:u8, one_position: u8) -> Vec<Complex<f64>> {
     let mut v = Vec::new();
     for i in 0..size {
         if i == one_position {
-            v.push(1u8);
+            v.push(Complex::new(1f64, 0f64));
         }else{
-            v.push(0u8);
+            v.push(Complex::new(0f64, 0f64));
         }
     }
     v
 
 }
 
-pub fn create_swap_matrix(total_qbits:u8, qbit_x:u8,qbit_y:u8) -> Vec<Vec<u8>>{
+pub fn  create_swap_matrix(total_qbits:u8, qbit_x:u8,qbit_y:u8, complexField: ComplexField) -> Matrix<ComplexField>{
     let dim = pow2(total_qbits);
     let x_rep = 1<<qbit_x;
     let y_rep = 1<<qbit_y;
@@ -32,28 +34,27 @@ pub fn create_swap_matrix(total_qbits:u8, qbit_x:u8,qbit_y:u8) -> Vec<Vec<u8>>{
         let shift_y = (shift_x ^ (i & y_rep)) | to_x;
         mat.push(get_column_for_one(dim, shift_y));
     }
-    mat
+    let matrix = Matrix::new(complexField, mat);
+    matrix.transpose()
 }
 
+fn print_real_matrix(mat:&Matrix<ComplexField>) {
+    for i in 0..mat.rows() {
+        for j in 0..mat.columns() {
+            print!("{:?} ", mat.value_at(i, j).re);
+        }
+        println!();
+    }
+}
 
 fn main() {
     let total_qbits = 4;
     let qbit_x = 1;
     let qbit_y = 2;
+    let complexField = ComplexField;
 
-    let mat = create_swap_matrix(total_qbits, qbit_x, qbit_y);
-    for i in 0..mat.len() {
-        for j in  0..mat.len() {
-            print!("{}",mat[j][i]);
-            if j < mat.len()-1{
-                print!(",");
-            }
-        }
-        if i < mat.len() -1 {
-            print!(";");
-        }
+    let mat = create_swap_matrix(total_qbits, qbit_x, qbit_y, complexField);
 
-    }
-    println!("");
+    print_real_matrix(&mat);
 
 }
