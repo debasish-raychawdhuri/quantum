09b7912198bc826f747fd7bcda78b5f3c42921c6
Debasish Ray Chawdhuri <debasish@geekyarticlesc.com>
2020-04-30 15:49:45 +0530

    corrected tests for Deutsch's algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -174,10 +174,10 @@ mod tests {
         let o = Complex::new(1f64, 0f64);
         let uf = Matrix::new(ComplexField,
         vec![
-            vec![z,z,o,z],
-            vec![z,z,z,o],
-            vec![o,z,z,z],
             vec![z,o,z,z],
+            vec![o,z,z,z],
+            vec![z,z,z,o],
+            vec![z,z,o,z],
         ]);
         let test = QCS::new(2).hadamard(0).hadamard(1).gate(uf, &vec![0,1]).hadamard(0).hadamard(1);
         let res = test.run_once(&test.value_to_tensor(0b01));
