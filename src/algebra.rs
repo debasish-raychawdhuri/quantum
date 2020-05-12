793918f583a659519959a7c2a174b92c17047448
Debasish Ray Chawdhuri <debasish@geekyarticlesc.com>
2020-05-12 14:36:22 +0530

    added stack_id

--- src/algebra.rs
+++ src/algebra.rs
@@ -1,38 +1,37 @@
 extern crate linearalgebra;
-use linearalgebra::Ring;
-use linearalgebra::Matrix;
 use linearalgebra::field::ComplexField;
+use linearalgebra::Matrix;
+use linearalgebra::Ring;
 use num_complex::Complex;
 use std::num;
 
-pub fn pow2(x:u8) -> u64 {
-    1u64<<(x as u64)
+pub fn pow2(x: u8) -> u64 {
+    1u64 << (x as u64)
 }
 
-fn get_column_for_one(size:u64, one_position: u64) -> Vec<Complex<f64>> {
+fn get_column_for_one(size: u64, one_position: u64) -> Vec<Complex<f64>> {
     let mut v = Vec::new();
     for i in 0..size {
         if i == one_position {
             v.push(Complex::new(1f64, 0f64));
-        }else{
+        } else {
             v.push(Complex::new(0f64, 0f64));
         }
     }
     v
-
 }
 
-fn linear_search(list:&[u8], value:u8) -> Option<u8> {
+fn linear_search(list: &[u8], value: u8) -> Option<u8> {
     for i in 0..list.len() {
         if list[i] == value {
-            return Some(i as u8)
+            return Some(i as u8);
         }
     }
     return None;
 }
 
 //returns a list of swaps required to send the entries to the position 0,1,..
-fn create_swap_sequence(total_qbits:u8, entries:&[u8], ) -> Vec<(u8,u8)> {
+fn create_swap_sequence(total_qbits: u8, entries: &[u8]) -> Vec<(u8, u8)> {
     let mut swaps = Vec::new();
     let mut currect_pos = Vec::new();
     for i in 0..total_qbits {
@@ -46,17 +45,22 @@ fn create_swap_sequence(total_qbits:u8, entries:&[u8], ) -> Vec<(u8,u8)> {
                 currect_pos.swap(pos as usize, next as usize);
             }
 
-            next+=1;
+            next += 1;
         }
     }
 
     swaps
 }
 
-pub fn  create_swap_matrix(total_qbits:u8, qbit_x:u8,qbit_y:u8, complex_field: ComplexField) -> Matrix<ComplexField>{
+pub fn create_swap_matrix(
+    total_qbits: u8,
+    qbit_x: u8,
+    qbit_y: u8,
+    complex_field: ComplexField,
+) -> Matrix<ComplexField> {
     let dim = pow2(total_qbits);
-    let x_rep = 1<<qbit_x;
-    let y_rep = 1<<qbit_y;
+    let x_rep = 1 << qbit_x;
+    let y_rep = 1 << qbit_y;
     let mut mat = Vec::new();
     for i in 0..dim {
         let bit_x = (i & x_rep) >> qbit_x;
@@ -71,51 +75,85 @@ pub fn  create_swap_matrix(total_qbits:u8, qbit_x:u8,qbit_y:u8, complex_field: C
     matrix.transpose()
 }
 
-pub fn create_swap_matrix_for_entries(total_qbits:u8, entries:&[u8]) -> Matrix<ComplexField> {
+pub fn create_swap_matrix_for_entries(total_qbits: u8, entries: &[u8]) -> Matrix<ComplexField> {
     let complex_field = ComplexField;
     let swap_seq = create_swap_sequence(total_qbits, entries);
-    let mut prod = create_swap_matrix(total_qbits,0,0,complex_field);
+    let mut prod = create_swap_matrix(total_qbits, 0, 0, complex_field);
 
     for swap in swap_seq {
-        prod = create_swap_matrix(total_qbits,swap.0,swap.1,complex_field).mul(&prod).unwrap();
+        prod = create_swap_matrix(total_qbits, swap.0, swap.1, complex_field)
+            .mul(&prod)
+            .unwrap();
     }
     prod
 }
 
-fn stack_mat(total_qbits:u8, matrix: Matrix<ComplexField>) -> Matrix<ComplexField> {
+fn stack_mat(total_qbits: u8, matrix: Matrix<ComplexField>) -> Matrix<ComplexField> {
+    if matrix.rows() != matrix.columns() {
+        panic!("Only square matrix supported");
+    }
     let ring = ComplexField;
-    let rows =pow2(total_qbits) as usize;
-    let mut data = vec![vec![ComplexField.zero();rows];rows];
-    for i in 0..rows{
-        for j in 0..rows{
-            let col_block = j/matrix.rows();
-            if i >= matrix.rows() * col_block && i <  matrix.rows() * (col_block+1) {
-                data[i][j] = matrix.value_at(i-matrix.rows() * col_block,j-matrix.rows() * col_block);
+    let rows = pow2(total_qbits) as usize;
+    let mut data = vec![vec![ComplexField.zero(); rows]; rows];
+    for i in 0..rows {
+        let row_block = i / matrix.rows();
+        for j in 0..rows {
+            let col_block = j / matrix.rows();
+
+            if row_block == col_block {
+                data[i][j] =
+                    matrix.value_at(i - matrix.rows() * col_block, j - matrix.rows() * col_block);
             }
         }
     }
     Matrix::new(ring, data)
 }
 
-
-pub fn conjugate_transpose(matrix: &Matrix<ComplexField>) -> Matrix<ComplexField> {
+fn stack_id(total_qbits: u8, matrix: Matrix<ComplexField>) -> Matrix<ComplexField> {
+    if matrix.rows() != matrix.columns() {
+        panic!("Only square matrix supported");
+    }
     let ring = ComplexField;
-    let rows = matrix.rows();
-    let mut data = vec![vec![ComplexField.zero();rows];rows];
-    for i in 0..rows{
-        for j in 0..rows{
-            data[i][j]=matrix.value_at(j, i).conj();
+    let rows = pow2(total_qbits) as usize;
+    let total_blocks = rows / matrix.rows();
+    let mut data = vec![vec![ComplexField.zero(); rows]; rows];
+    for i in 0..rows {
+        let row_block = i / matrix.rows();
+        for j in 0..rows {
+            let col_block = j / matrix.rows();
+            if row_block == col_block && col_block == total_blocks - 1 {
+                data[i][j] =
+                    matrix.value_at(i - matrix.rows() * col_block, j - matrix.rows() * col_block);
+            } else if i == j {
+                data[i][j] = ring.one();
+            }
         }
     }
     Matrix::new(ring, data)
 }
-pub fn apply_matrix_to(total_qbits:u8, matrix: &Matrix<ComplexField>, bits_to_apply2:&[u8]) -> Matrix<ComplexField> {
+
+pub fn conjugate_transpose(matrix: &Matrix<ComplexField>) -> Matrix<ComplexField> {
+    let ring = ComplexField;
+    let rows = matrix.rows();
+    let mut data = vec![vec![ComplexField.zero(); rows]; rows];
+    for i in 0..rows {
+        for j in 0..rows {
+            data[i][j] = matrix.value_at(j, i).conj();
+        }
+    }
+    Matrix::new(ring, data)
+}
+pub fn apply_matrix_to(
+    total_qbits: u8,
+    matrix: &Matrix<ComplexField>,
+    bits_to_apply2: &[u8],
+) -> Matrix<ComplexField> {
     let swaps = create_swap_matrix_for_entries(total_qbits, bits_to_apply2);
     let inv_swaps = conjugate_transpose(&swaps);
     let ramped_mat = stack_mat(total_qbits, matrix.clone());
     inv_swaps.mul(&ramped_mat.mul(&swaps).unwrap()).unwrap()
 }
-fn print_real_matrix(mat:&Matrix<ComplexField>) {
+fn print_real_matrix(mat: &Matrix<ComplexField>) {
     for i in 0..mat.rows() {
         for j in 0..mat.columns() {
             print!("{:.2?} ", mat.value_at(i, j).re);
--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,71 @@
 mod algebra;
 use crate::algebra::pow2;
-use linearalgebra::Matrix;
-use linearalgebra::field::ComplexField;
 use algebra::*;
+use linearalgebra::field::ComplexField;
+use linearalgebra::Matrix;
 use num_complex::Complex;
 extern crate rand;
 use rand::prelude::*;
 
 #[derive(PartialEq, Clone, Debug)]
-pub struct QCS{
-    total_qbits:u8,
-    matrix: Matrix<ComplexField>
+pub struct QCS {
+    total_qbits: u8,
+    matrix: Matrix<ComplexField>,
 }
 
-
 impl QCS {
-    pub fn new(total_qbits:u8) -> Self {
+    pub fn new(total_qbits: u8) -> Self {
         let complex_field = ComplexField;
         let matrix = create_swap_matrix(total_qbits, 0, 0, complex_field);
         QCS {
             total_qbits,
-            matrix
+            matrix,
         }
     }
 
-    pub fn gate(&self, gate:Matrix<ComplexField>, wires: &[u8]) -> QCS {
-
+    pub fn gate(&self, gate: Matrix<ComplexField>, wires: &[u8]) -> QCS {
         let matrix = apply_matrix_to(self.total_qbits, &gate, wires);
         QCS {
-            total_qbits:self.total_qbits,
-            matrix: matrix.mul(&self.matrix).unwrap()
+            total_qbits: self.total_qbits,
+            matrix: matrix.mul(&self.matrix).unwrap(),
         }
     }
 
-    pub fn hadamard(&self, wire:u8) -> QCS {
-        let hp = Complex::new(1f64/2f64.sqrt(), 0f64);
-        self.gate(Matrix::new(ComplexField, vec![vec![hp,hp],vec![hp,-hp]]),&[wire])
+    pub fn hadamard(&self, wire: u8) -> QCS {
+        let hp = Complex::new(1f64 / 2f64.sqrt(), 0f64);
+        self.gate(
+            Matrix::new(ComplexField, vec![vec![hp, hp], vec![hp, -hp]]),
+            &[wire],
+        )
     }
 
-    pub fn not(&self, wire:u8) -> QCS {
-        let z = Complex::new(0f64,0f64);
+    pub fn not(&self, wire: u8) -> QCS {
+        let z = Complex::new(0f64, 0f64);
         let o = Complex::new(1f64, 0f64);
-        let not_gate = Matrix::new(ComplexField,vec![vec![z,o,o,z]]);
-        self.gate(not_gate,&[wire])
+        let not_gate = Matrix::new(ComplexField, vec![vec![z, o, o, z]]);
+        self.gate(not_gate, &[wire])
     }
 
-    pub fn cnot(&self, control:u8, wire:u8) -> QCS{
-        let z = Complex::new(0f64,0f64);
+    pub fn cnot(&self, control: u8, wire: u8) -> QCS {
+        let z = Complex::new(0f64, 0f64);
         let o = Complex::new(1f64, 0f64);
-        let cnot_gate = Matrix::new(ComplexField,
-        vec![
-            vec![o,z,z,z],
-            vec![z,o,z,z],
-            vec![z,z,z,o],
-            vec![z,z,o,z],
-        ]);
-        self.gate(cnot_gate,&[wire, control])
+        let cnot_gate = Matrix::new(
+            ComplexField,
+            vec![
+                vec![o, z, z, z],
+                vec![z, o, z, z],
+                vec![z, z, z, o],
+                vec![z, z, o, z],
+            ],
+        );
+        self.gate(cnot_gate, &[wire, control])
     }
 
-    pub fn swap(&self, x:u8, y:u8) -> QCS {
-        let matrix = create_swap_matrix(self.total_qbits, x, y , ComplexField);
-        QCS{
-            total_qbits:self.total_qbits,
-            matrix
+    pub fn swap(&self, x: u8, y: u8) -> QCS {
+        let matrix = create_swap_matrix(self.total_qbits, x, y, ComplexField);
+        QCS {
+            total_qbits: self.total_qbits,
+            matrix,
         }
     }
 
@@ -71,49 +74,45 @@ impl QCS {
         for i in 0..input.len() {
             data.push(vec![input[i]]);
         }
-        let input_mat = Matrix::new(ComplexField,data);
+        let input_mat = Matrix::new(ComplexField, data);
         let output_mat = self.matrix.mul(&input_mat).unwrap();
         let mut out = Vec::new();
         for i in 0..input.len() {
-            out.push(output_mat.value_at(i,0));
+            out.push(output_mat.value_at(i, 0));
         }
         out
     }
 
-    pub fn value_to_bit_vector(&self,value:usize) -> Vec<u8> {
+    pub fn value_to_bit_vector(&self, value: usize) -> Vec<u8> {
         let mut v = value;
         let mut result = Vec::new();
         for _ in 0..self.total_qbits {
             result.push((v % 2) as u8);
-            v = v/2;
+            v = v / 2;
         }
         result
     }
 
-    pub fn value_to_tensor(&self, value:usize) -> Vec<Complex<f64>> {
+    pub fn value_to_tensor(&self, value: usize) -> Vec<Complex<f64>> {
         let mut result = Vec::new();
-        let z = Complex::new(0f64,0f64);
+        let z = Complex::new(0f64, 0f64);
         let o = Complex::new(1f64, 0f64);
-        for i in 0..pow2(self.total_qbits){
+        for i in 0..pow2(self.total_qbits) {
             if value == i as usize {
                 result.push(o);
-            }else {
+            } else {
                 result.push(z);
             }
         }
         result
     }
 
-
-
     pub fn run_once(&self, input: &[Complex<f64>]) -> Vec<u8> {
-        let output = self.run( input);
+        let output = self.run(input);
         let mut rng = rand::thread_rng();
-        let probabilities:Vec<f64> = output.into_iter().map(|x|{
-             ((x)*(x.conj())).re
-        }).collect();
+        let probabilities: Vec<f64> = output.into_iter().map(|x| ((x) * (x.conj())).re).collect();
 
-        let y:f64 = rng.gen();
+        let y: f64 = rng.gen();
         let mut sum = 0f64;
         for i in 0..input.len() {
             sum += probabilities[i];
@@ -122,7 +121,6 @@ impl QCS {
             }
         }
         return self.value_to_bit_vector(input.len());
-
     }
 }
 
@@ -131,88 +129,116 @@ mod tests {
     use super::*;
     #[test]
     fn test_cnot() {
-        let s_not = QCS::new(3).cnot(1, 2).cnot(2,1).cnot(1,2);
-        let swap = QCS::new(3).swap(2,1);
+        let s_not = QCS::new(3).cnot(1, 2).cnot(2, 1).cnot(1, 2);
+        let swap = QCS::new(3).swap(2, 1);
         assert_eq!(s_not, swap);
     }
 
     #[test]
     fn test_swap() {
-        let swap = QCS::new(4).swap(2,1);
+        let swap = QCS::new(4).swap(2, 1);
         let res = swap.run_once(&swap.value_to_tensor(0b010));
         let exp = swap.value_to_bit_vector(0b100);
         assert_eq!(res, exp);
     }
     #[test]
-    fn test_value_to_tensor(){
+    fn test_value_to_tensor() {
         let test = QCS::new(2);
         let res = test.value_to_tensor(0b00);
-        let z = Complex::new(0f64,0f64);
+        let z = Complex::new(0f64, 0f64);
         let o = Complex::new(1f64, 0f64);
-        assert_eq!(res, vec![o,z,z,z]);
+        assert_eq!(res, vec![o, z, z, z]);
         let res = test.value_to_tensor(0b01);
-        assert_eq!(res, vec![z,o,z,z]);
+        assert_eq!(res, vec![z, o, z, z]);
     }
     #[test]
     fn test_deutsch_0() {
-        let z = Complex::new(0f64,0f64);
+        let z = Complex::new(0f64, 0f64);
         let o = Complex::new(1f64, 0f64);
-        let uf = Matrix::new(ComplexField,
-        vec![
-            vec![o,z,z,z],
-            vec![z,o,z,z],
-            vec![z,z,o,z],
-            vec![z,z,z,o],
-        ]);
-        let test = QCS::new(2).hadamard(0).hadamard(1).gate(uf, &vec![0,1]).hadamard(0).hadamard(1);
+        let uf = Matrix::new(
+            ComplexField,
+            vec![
+                vec![o, z, z, z],
+                vec![z, o, z, z],
+                vec![z, z, o, z],
+                vec![z, z, z, o],
+            ],
+        );
+        let test = QCS::new(2)
+            .hadamard(0)
+            .hadamard(1)
+            .gate(uf, &vec![0, 1])
+            .hadamard(0)
+            .hadamard(1);
         let res = test.run_once(&test.value_to_tensor(0b01));
-        assert_eq!(res, vec![1,0]);
+        assert_eq!(res, vec![1, 0]);
     }
 
     #[test]
     fn test_deutsch_1() {
-        let z = Complex::new(0f64,0f64);
+        let z = Complex::new(0f64, 0f64);
         let o = Complex::new(1f64, 0f64);
-        let uf = Matrix::new(ComplexField,
-        vec![
-            vec![z,o,z,z],
-            vec![o,z,z,z],
-            vec![z,z,z,o],
-            vec![z,z,o,z],
-        ]);
-        let test = QCS::new(2).hadamard(0).hadamard(1).gate(uf, &vec![0,1]).hadamard(0).hadamard(1);
+        let uf = Matrix::new(
+            ComplexField,
+            vec![
+                vec![z, o, z, z],
+                vec![o, z, z, z],
+                vec![z, z, z, o],
+                vec![z, z, o, z],
+            ],
+        );
+        let test = QCS::new(2)
+            .hadamard(0)
+            .hadamard(1)
+            .gate(uf, &vec![0, 1])
+            .hadamard(0)
+            .hadamard(1);
         let res = test.run_once(&test.value_to_tensor(0b01));
-        assert_eq!(res, vec![1,0]);
+        assert_eq!(res, vec![1, 0]);
     }
 
     #[test]
     fn test_deutsch_i() {
-        let z = Complex::new(0f64,0f64);
+        let z = Complex::new(0f64, 0f64);
         let o = Complex::new(1f64, 0f64);
-        let uf = Matrix::new(ComplexField,
-        vec![
-            vec![o,z,z,z],
-            vec![z,o,z,z],
-            vec![z,z,z,o],
-            vec![z,z,o,z],
-        ]);
-        let test = QCS::new(2).hadamard(0).hadamard(1).gate(uf, &vec![0,1]).hadamard(0).hadamard(1);
+        let uf = Matrix::new(
+            ComplexField,
+            vec![
+                vec![o, z, z, z],
+                vec![z, o, z, z],
+                vec![z, z, z, o],
+                vec![z, z, o, z],
+            ],
+        );
+        let test = QCS::new(2)
+            .hadamard(0)
+            .hadamard(1)
+            .gate(uf, &vec![0, 1])
+            .hadamard(0)
+            .hadamard(1);
         let res = test.run_once(&test.value_to_tensor(0b01));
-        assert_eq!(res, vec![1,1]);
+        assert_eq!(res, vec![1, 1]);
     }
     #[test]
     fn test_deutsch_not() {
-        let z = Complex::new(0f64,0f64);
+        let z = Complex::new(0f64, 0f64);
         let o = Complex::new(1f64, 0f64);
-        let uf = Matrix::new(ComplexField,
-        vec![
-            vec![z,o,z,z],
-            vec![o,z,z,z],
-            vec![z,z,o,z],
-            vec![z,z,z,o],
-        ]);
-        let test = QCS::new(2).hadamard(0).hadamard(1).gate(uf, &vec![0,1]).hadamard(0).hadamard(1);
+        let uf = Matrix::new(
+            ComplexField,
+            vec![
+                vec![z, o, z, z],
+                vec![o, z, z, z],
+                vec![z, z, o, z],
+                vec![z, z, z, o],
+            ],
+        );
+        let test = QCS::new(2)
+            .hadamard(0)
+            .hadamard(1)
+            .gate(uf, &vec![0, 1])
+            .hadamard(0)
+            .hadamard(1);
         let res = test.run_once(&test.value_to_tensor(0b01));
-        assert_eq!(res, vec![1,1]);
+        assert_eq!(res, vec![1, 1]);
     }
 }
